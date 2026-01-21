8ec2f119516c2b6dc2a82aaad3d8f21b61353556
DipanMandal <mandaldipan2611@gmail.com>
2026-01-21 13:13:36 +0530

    updated config

--- model/config.py
+++ model/config.py
@@ -5,4 +5,6 @@ class NanoLMConfig():
         self.dropout = 0.2
         self.num_heads = 1
         self.d_hidden = 8
+        self.vocab_size = 500
+        self.n_encoder_layers = 2
         self.use_bias = True
